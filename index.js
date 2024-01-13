89c26c2779e7dce164b640ca1fd1e36fee7f8ac3
pratikkshirsagar <meetpratiik1995@gmail.com>
2024-01-13 21:36:16 +0530

    db connect done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('bye');
+const app = require('./src/app');
+const pool = require('./src/pool');
+
+require('dotenv').config();
+
+pool
+  .connect({
+    host: `${process.env.host}`,
+    port: process.env.port,
+    database: `${process.env.database}`,
+    user: `${process.env.user}`,
+    password: `${process.env.password}`,
+  })
+  .then((result) => {
+    app().listen(3005, () => {
+      console.log('Listening on port 3005');
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
