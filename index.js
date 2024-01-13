a6574560ba86d0c138488d541105a26091436ca4
pratikkshirsagar <meetpratiik1995@gmail.com>
2024-01-14 00:48:24 +0530

    fix

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const pool = require('./src/pool');
 
 require('dotenv').config();
 
-pool
-  .connect({
-    host: `${process.env.host}`,
-    port: process.env.port,
-    database: `${process.env.database}`,
-    user: `${process.env.user}`,
-    password: `${process.env.password}`,
-  })
-  .then((result) => {
+const dbConnect = async () => {
+  try {
+    await pool.connect({
+      host: `${process.env.host}`,
+      port: process.env.port,
+      database: `${process.env.database}`,
+      user: `${process.env.user}`,
+      password: `${process.env.password}`,
+    });
+
     app().listen(3005, () => {
       console.log('Listening on port 3005');
     });
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error(err);
-  });
+  }
+};
+
+dbConnect();
