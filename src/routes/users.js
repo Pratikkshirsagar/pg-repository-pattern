0e6c5d0ac03cd4679496c15188d096156915291d
pratikkshirsagar <meetpratiik1995@gmail.com>
2024-01-14 16:35:06 +0530

    apis added

--- src/routes/users.js
+++ src/routes/users.js
@@ -20,10 +20,37 @@ router.get('/users/:id', async (req, res) => {
   res.send(user);
 });
 
-router.post('/users', async (req, res) => {});
+router.post('/users', async (req, res) => {
+  const { username, bio } = req.body;
 
-router.put('/users/:id', async (req, res) => {});
+  const user = await UserRepo.insert(username, bio);
 
-router.delete('/users/:id', async (req, res) => {});
+  res.send(user);
+});
+
+router.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { username, bio } = req.body;
+
+  const user = await UserRepo.update(id, username, bio);
+
+  if (!user) {
+    res.sendStatus(404);
+  }
+
+  res.send(user);
+});
+
+router.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const user = await UserRepo.delete(id);
+
+  if (!user) {
+    res.sendStatus(404);
+  }
+
+  res.send(user);
+});
 
 module.exports = router;
